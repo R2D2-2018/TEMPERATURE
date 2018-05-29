d7e51316d9f30729b852e43fab73cb54ddcbf5b0
Michelbaartman <Michelbaartman@users.noreply.github.com>
2018-05-29 16:05:02 +0200

    Able to start a transmission with a DHT sensor, but the DHT22 returns only high values as data.

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,53 @@
 #include "dht22.hpp"
 #include "wrap-hwlib.hpp"
 
+int read(hwlib::target::pin_in_out pin);
+
 int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
 
-    hwlib::cout << "setup pins\n";
-    hwlib::wait_ms(1100);
-    //auto pin = hwlib::target::pin_in(hwlib::target::pins::d24);
-    auto pin = hwlib::target::pin_in_out(hwlib::target::pins::d7);
-    hwlib::wait_ms(1100);
-    DHT22 dht22(24);
-
-    //uint16_t data = 0;
-    //int waitTime = 0;
-
+    hwlib::wait_ms(1200);
+    auto pin = hwlib::target::pin_in_out(hwlib::target::pins::a0);
     while(true){
-        pin.direction_set_output();
-        //hwlib::cout << "set start bit\n";
-        pin.set(0);
-        hwlib::wait_ms(20);
-        pin.set(1);
-        pin.direction_set_input();
-        while(pin.get() == 1){
-            hwlib::cout << "waiting\n";
-            hwlib::wait_us(10);
-        }
-        hwlib::cout << "start transmission\n";
-        while(true){
-        }
-        //pin.refresh();
-        //hwlib::wait_ms(18);
-        //pin.flush();
-        //hwlib::wait_ms(1100);
-        //pin = hwlib::target::pin_in(hwlib::target::pins::d24);
-        // while(true){
-        //     if(pin.get() == 0){
-        //         hwlib::cout << "0";
-        //     }
-        //     //hwlib::cout << pin.get();
-        //     //hwlib::wait_us(1);
-        //     //hwlib::wait_ms(100);
-        // }
-        // int waitTime;
-        // for(int i = 0; i < 8; i++){
-        //     waitTime = 0;
-        //     while(pin.get() == 1){}
-        //     waitTime ++;
-        // }
+        read(pin);
+        hwlib::wait_ms(2000);
     }
 
+    return 0;
+}
+
+int read(hwlib::target::pin_in_out pin){
+    
+
+
+    pin.direction_set_output();
+    int waitTime = 0;
+    int bit = 0;
+    int byte = 1;
+    pin.set(0);
+    hwlib::wait_ms(20);
+    pin.set(1);
+    hwlib::wait_us(20);
+    pin.direction_set_input();
+    while(pin.get() != 0){}
+    //hwlib::cout << "start communication\n";
+    while(pin.get() != 1){}
+    while(true){
+        while(pin.get() != 0){
+            waitTime ++;
+            hwlib::wait_us(1);
+            if(waitTime > 90){ hwlib::cout << "time out"; return 0; }
+        }
+        if(waitTime > 30){
+            bit = 1;
+        } else { 
+            bit = 0;
+        }
+        byte = (byte*10)+bit;
+        if(byte > 100000){
+            hwlib::cout << byte;
+            return 0;
+        }
+    }
     return 0;
 }
