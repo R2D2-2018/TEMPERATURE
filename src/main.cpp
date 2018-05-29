eee140a8b5e501d2d6ffb3565334708ec397a8d7
Michelbaartman <Michelbaartman@users.noreply.github.com>
2018-05-29 17:26:44 +0200

    Another attempt at communication, however this time to a DHT11. didn't change much. still only getting high values as data after start transmission.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
 
     hwlib::wait_ms(1200);
-    auto pin = hwlib::target::pin_in_out(hwlib::target::pins::a0);
+    auto pin = due::pin_in_out(due::pins::a0);
     while(true){
         read(pin);
         hwlib::wait_ms(2000);
@@ -16,38 +16,50 @@ int main() {
     return 0;
 }
 
-int read(hwlib::target::pin_in_out pin){
-    
-
-
+int read(due::pin_in_out pin){
+    int waitTime1 = 0;
+    int waitTime2 = 0;
+    pin.refresh(); pin.flush();
+    // start transmission command
     pin.direction_set_output();
-    int waitTime = 0;
-    int bit = 0;
-    int byte = 1;
     pin.set(0);
-    hwlib::wait_ms(20);
+    hwlib::wait_ms(18);
     pin.set(1);
-    hwlib::wait_us(20);
     pin.direction_set_input();
-    while(pin.get() != 0){}
-    //hwlib::cout << "start communication\n";
-    while(pin.get() != 1){}
-    while(true){
-        while(pin.get() != 0){
-            waitTime ++;
-            hwlib::wait_us(1);
-            if(waitTime > 90){ hwlib::cout << "time out"; return 0; }
-        }
-        if(waitTime > 30){
-            bit = 1;
-        } else { 
-            bit = 0;
-        }
-        byte = (byte*10)+bit;
-        if(byte > 100000){
-            hwlib::cout << byte;
-            return 0;
-        }
+    //hwlib::wait_us(20);
+
+    //waiting for transmittion from DHT11
+    while(pin.get() != 0){ waitTime1 ++; }
+    while(pin.get() != 1){ waitTime2 ++; }
+    hwlib::cout << "\n" << waitTime1 << "-" << waitTime2 << "\n";
+    hwlib::cout << "start transmission\n";
+
+    waitTime1 = 0;
+    while(waitTime1 < 40){
+        hwlib::cout << pin.get();
+        waitTime1 ++;
     }
+
+    // int count = 0;
+    // while(count < 8){
+    //     while(true){
+    //         //hwlib::cout << "bit " << count;
+    //         waitTime = 0;
+    //         while(pin.get() != 0){}
+    //         while(pin.get() != 1){
+    //             waitTime++;
+    //             hwlib::cout << "waiting";
+    //             hwlib::wait_us(1);
+    //         }
+    //         if(waitTime > 40){
+    //             hwlib::cout << "0";
+    //             break;
+    //         }
+    //         hwlib::cout << "1";
+    //         break;
+    //         //hwlib::cout << "bit got";
+    //     }
+    //     count++;
+    // }
     return 0;
 }
